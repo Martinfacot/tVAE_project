c4f90e1745b9ed26474402ccd271abed9cb0dc25
Martinfacot <mfacot@gmail.com>
2025-03-19 14:57:48 +0100

    new version :)

---
+++ test_mini_tvae/mini_tvae_v2.py
@@ -0,0 +1,255 @@
+"""Miniaturized TVAE module."""
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.nn import Linear, Module, Parameter, ReLU, Sequential
+from torch.nn.functional import cross_entropy
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+
+class Encoder(Module):
+    """Encoder for the TVAE."""
+    def __init__(self, data_dim, compress_dims, embedding_dim):
+        super(Encoder, self).__init__()
+        dim = data_dim
+        seq = []
+        for item in list(compress_dims):
+            seq += [Linear(dim, item), ReLU()]
+            dim = item
+
+        self.seq = Sequential(*seq)
+        self.fc1 = Linear(dim, embedding_dim)
+        self.fc2 = Linear(dim, embedding_dim)
+
+    def forward(self, input_):
+        """Encode the passed `input_`."""
+        feature = self.seq(input_)
+        mu = self.fc1(feature)
+        logvar = self.fc2(feature)
+        std = torch.exp(0.5 * logvar)
+        return mu, std, logvar
+
+
+class Decoder(Module):
+    """Decoder for the TVAE."""
+    def __init__(self, embedding_dim, decompress_dims, data_dim):
+        super(Decoder, self).__init__()
+        dim = embedding_dim
+        seq = []
+        for item in list(decompress_dims):
+            seq += [Linear(dim, item), ReLU()]
+            dim = item
+
+        seq.append(Linear(dim, data_dim))
+        self.seq = Sequential(*seq)
+        self.sigma = Parameter(torch.ones(data_dim) * 0.1)
+
+    def forward(self, input_):
+        """Decode the passed `input_`."""
+        return self.seq(input_), self.sigma
+
+
+def _loss_function(recon_x, x, sigmas, mu, logvar, output_info, factor):
+    """recon_x: The data reconstructed by the decoder.
+
+x: Original input data.
+
+sigmas: Standard deviations for continuous columns.
+
+mu: The mean of the latent distribution.
+
+logvar: The logarithm of the variance of the latent distribution.
+
+output_info: A list containing information about the transformed columns (e.g. whether they are continuous or discrete).
+
+factor: A weighting factor for the reconstruction loss."""
+
+    st = 0
+    loss = []
+    for column_info in output_info:
+        for span_info in column_info:
+            if span_info.activation_fn != 'softmax':
+                # Continuous
+                ed = st + span_info.dim
+                std = sigmas[st] # standard deviation
+                eq = x[:, st] - torch.tanh(recon_x[:, st]) 
+                # error between the original value and the reconstructed value + tanh bring the reconstructed values into the interval [-1, 1]
+                loss.append((eq**2 / 2 / (std**2)).sum())
+                # root-MSE loss
+                loss.append(torch.log(std) * x.size()[0])
+                st = ed
+            else:
+                # Categorical with softmax
+                ed = st + span_info.dim
+                loss.append(
+                    cross_entropy(
+                        recon_x[:, st:ed], torch.argmax(x[:, st:ed], dim=-1), reduction='sum'
+                    )
+                )
+                st = ed
+
+    assert st == recon_x.size()[1]
+    KLD = -0.5 * torch.sum(1 + logvar - mu**2 - logvar.exp())
+    return sum(loss) * factor / x.size()[0], KLD / x.size()[0]
+
+
+class MiniTVAE:
+    """Miniaturized TVAE implementation."""
+
+    def __init__(
+        # default hyperparameters
+        self,
+        embedding_dim=128,
+        compress_dims=(128, 128),
+        decompress_dims=(128, 128),
+        l2scale=1e-5,
+        batch_size=500,
+        epochs=300,
+        loss_factor=2,
+        cuda=True,
+        verbose=False,
+    ):
+        self.embedding_dim = embedding_dim
+        self.compress_dims = compress_dims
+        self.decompress_dims = decompress_dims
+        self.l2scale = l2scale
+        self.batch_size = batch_size
+        self.loss_factor = loss_factor
+        self.epochs = epochs
+        self.loss_values = pd.DataFrame(columns=['Epoch', 'Batch', 'loss_1', 'loss_2', 'total_loss'])
+        self.verbose = verbose
+
+        if not cuda or not torch.cuda.is_available():
+            device = 'cpu'
+        elif isinstance(cuda, str):
+            device = cuda
+        else:
+            device = 'cuda'
+
+        self._device = torch.device(device)
+
+    def fit(self, train_data, discrete_columns=()):
+        """Fit the TVAE model to the training data."""
+        from mini_data_trans import DataTransformer
+        
+        self.transformer = DataTransformer()
+        self.transformer.fit(train_data, discrete_columns)
+        
+        try:
+            train_data_t = self.transformer.transform(train_data)
+        except Exception as e:
+            print(f"Error transforming data: {e}")
+            print("Trying with more robust error handling...")
+            
+            # Try again with the more robust implementation
+            train_data_t = np.zeros((len(train_data), self.transformer.output_dimensions))
+            st = 0
+            for column_transform_info in self.transformer._column_transform_info_list:
+                column_name = column_transform_info.column_name
+                data = train_data[[column_name]]
+                
+                try:
+                    if column_transform_info.column_type == 'continuous':
+                        transformed = self.transformer._transform_continuous(column_transform_info, data)
+                    else:
+                        transformed = self.transformer._transform_discrete(column_transform_info, data)
+                        
+                    dim = column_transform_info.output_dimensions
+                    train_data_t[:, st:st+dim] = transformed
+                    st += dim
+                except Exception as column_error:
+                    print(f"Error transforming column '{column_name}': {column_error}")
+                    # Skip this column and continue with others
+                    dim = column_transform_info.output_dimensions
+                    st += dim
+        
+        dataset = TensorDataset(torch.from_numpy(train_data_t.astype('float32')).to(self._device))
+        loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True, drop_last=False)
+
+        data_dim = self.transformer.output_dimensions
+        encoder = Encoder(data_dim, self.compress_dims, self.embedding_dim).to(self._device)
+        self.decoder = Decoder(self.embedding_dim, self.decompress_dims, data_dim).to(self._device)
+        optimizerAE = Adam(
+            list(encoder.parameters()) + list(self.decoder.parameters()), weight_decay=self.l2scale
+        )
+
+        # Initialize DataFrame to store loss values with all required columns
+        self.loss_values = pd.DataFrame(columns=['Epoch', 'Batch', 'loss_1', 'loss_2', 'total_loss'])
+        
+        iterator = tqdm(range(self.epochs), disable=(not self.verbose))
+        if self.verbose:
+            iterator_description = 'Loss: {loss:.3f}'
+            iterator.set_description(iterator_description.format(loss=0))
+
+        for i in iterator:
+            batch_losses = []
+            
+            for id_, data in enumerate(loader):
+                optimizerAE.zero_grad()
+                real = data[0].to(self._device)
+                mu, std, logvar = encoder(real)
+                eps = torch.randn_like(std)
+                emb = eps * std + mu
+                rec, sigmas = self.decoder(emb)
+                loss_1, loss_2 = _loss_function(
+                    rec,
+                    real,
+                    sigmas,
+                    mu,
+                    logvar,
+                    self.transformer.output_info_list,
+                    self.loss_factor,
+                )
+                total_loss = loss_1 + loss_2  # total loss is the sum of reconstruction loss and KLD loss
+                total_loss.backward()
+                optimizerAE.step()
+                self.decoder.sigma.data.clamp_(0.01, 1.0)
+
+                # Store all loss components
+                batch_losses.append({
+                    'Epoch': i,
+                    'Batch': id_,
+                    'loss_1': loss_1.detach().cpu().item(),
+                    'loss_2': loss_2.detach().cpu().item(),
+                    'total_loss': total_loss.detach().cpu().item()
+                })
+
+            # Convert batch losses to DataFrame and append to loss_values
+            epoch_loss_df = pd.DataFrame(batch_losses)
+            if not self.loss_values.empty:
+                self.loss_values = pd.concat([self.loss_values, epoch_loss_df]).reset_index(drop=True)
+            else:
+                self.loss_values = epoch_loss_df
+
+            if self.verbose:
+                # Use the total_loss for the progress bar
+                avg_total_loss = epoch_loss_df['total_loss'].mean()
+                iterator.set_description(iterator_description.format(loss=avg_total_loss))
+
+    def sample(self, samples):
+        """Sample synthetic data similar to the training data.
+
+        Args:
+            samples (int):
+                Number of rows to sample.
+
+        Returns:
+            numpy.ndarray or pandas.DataFrame
+        """
+        self.decoder.eval()
+
+        steps = samples // self.batch_size + 1
+        data = []
+        for _ in range(steps):
+            mean = torch.zeros(self.batch_size, self.embedding_dim)
+            std = mean + 1
+            noise = torch.normal(mean=mean, std=std).to(self._device)
+            fake, sigmas = self.decoder(noise)
+            fake = torch.tanh(fake)
+            data.append(fake.detach().cpu().numpy())
+
+        data = np.concatenate(data, axis=0)
+        data = data[:samples]
+        return self.transformer.inverse_transform(data, sigmas.detach().cpu().numpy())
