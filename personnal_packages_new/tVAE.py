190f3be229d10a82aaa32623ede601c543e57803
Martinfacot <mfacot@gmail.com>
2025-04-01 11:23:16 +0200

    fix import issue

--- personnal_packages_new/tVAE.py
+++ personnal_packages_new/tVAE.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
 from .data_trans import DataTransformer
-from ctgan.synthesizers.base import BaseSynthesizer, random_state
+from .BaseTrans import BaseSynthesizer, random_state
 
 
 class Encoder(Module):
