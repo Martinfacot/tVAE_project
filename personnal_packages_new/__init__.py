15576c37e73cebd9d7a78a9f6a17e2c744232337
Martinfacot <mfacot@gmail.com>
2025-04-02 09:02:18 +0200

    fix import issue

--- personnal_packages_new/__init__.py
+++ personnal_packages_new/__init__.py
@@ -1,9 +1,9 @@
-from .CBN import ClusterBasedNormalizer, FloatFormatter
 from .data_load import read_csv
 from .data_trans import DataTransformer, SpanInfo, ColumnTransformInfo
 from .NullTrans import NullTransformer
 from .BaseTrans import BaseTransformer
-from .OHE import OneHotEncoder
+from .CategoricalTransformer import OneHotEncoder
+from .NumericalTransformer import ClusterBasedNormalizer, FloatFormatter
 from .tVAE import TVAE, Encoder, Decoder
 
 __all__ = [
